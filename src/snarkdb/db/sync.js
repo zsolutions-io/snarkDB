4f1c59137ea673054d5d69a936d398ddd5cc37a3
Pierre-André Long <accounts@makerare.io>
2024-03-20 18:34:04 +0100

    .

--- src/snarkdb/db/sync.js
+++ src/snarkdb/db/sync.js
@@ -159,7 +159,7 @@ export async function sync_queries() {
     const files = await fs.readdir(approved_dir);
     console.log(`Processing ${files.length} approved queries...`);
     for (const file of files) {
-      const query_id = file.split('.').pop().join('.');
+      const query_id = file.split('.').at(-1);
       await process_query(query_id);
       await fs.rm(approved_dir + "/" + file);
     }
