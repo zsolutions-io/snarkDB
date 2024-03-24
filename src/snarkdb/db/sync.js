3f087234f9d27f2e45722112e7df1f52e0798ac0
Pierre-André Long <accounts@makerare.io>
2024-03-24 22:30:50 +0100

    .

--- src/snarkdb/db/sync.js
+++ src/snarkdb/db/sync.js
@@ -432,10 +432,10 @@ async function sync_local_to_remote(local_dir_path, remote_path, ipfs_fs) {
   const remote = await get_all_remote_files(ipfs_fs, remote_path)
   const local_cid = local.cid?.toString == null ? null : local.cid.toString();
   const remote_cid = remote.cid?.toString == null ? null : remote.cid.toString();
-  console.log({ local_cid, remote_cid })
   if (local_cid === remote_cid) {
     return;
   }
+  console.log(local_dir_path)
   console.log(local.files)
   console.log(remote.files)
   const to_add = local.files.filter((file) => {
